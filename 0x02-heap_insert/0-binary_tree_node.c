fb7105d58324f43df79f755e3c4152008b5b870d
akeemseymens <812@holbertonschool.com>
2020-05-05 07:35:42 +0000

    added 0x02 files and gitignore types

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: the address of the parent node
+ * @value: the value to store in the node
+ *
+ * Return: If memory allocation fails, return NULL.
+ * Otherwise, return a pointer to the new node.
+ **/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = calloc(1, sizeof(*node));
+
+    if (!node)
+        return (NULL);
+
+	if (node)
+	{
+		node->n = value;
+		node->parent = parent;
+	}
+	return (node);
+}
