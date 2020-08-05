41a27755a5e9b8e623263ee9d344261fb1837e4e
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-08-04 19:06:19 -0700

    Update 0-binary_tree_node.c
    added new left and right to be null.

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -5,17 +5,21 @@
  *
  * @parent: parent of the new node
  * @value: value to be stored in the new node
+ *
  * Return: pointer to newly created node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = calloc(1, sizeof(*new));
+	binary_tree_t *new = malloc(sizeof(binary_tree_t);
 
-	if (!new)
+	if (!new_node)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	return (new);
 }
