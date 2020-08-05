8173b3b732c1eced96b912c85043a34fcc8122eb
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-08-04 19:12:28 -0700

    Update 0-binary_tree_node.c
    change variables to old ones.

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -13,13 +13,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t);
 
-	if (!new_node)
+	if (!new)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
 	return (new);
 }
