cef5816d4e92d8d4560af69234c7e477049ade89
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-12-09 14:26:19 -0800

    Update 0-lockboxes.py
    spacing and took out comments to test.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,12 +3,11 @@
 
 
 def canUnlockAll(boxes):
-  '''check if user and unlock all boxes'''
-  visited = set()
-  queue = set((0,))
-  while len(queue) > 0:
-    key = queue.pop()
-    if key < len(boxes):
-      visited.update((key,))
-      queue.update(set(boxes[key]).difference(visited))
-  return len(visited) == len(boxes)
+    visited = set()
+    queue = set((0,))
+    while len(queue) > 0:
+        key = queue.pop()
+        if key < len(boxes):
+            visited.update((key,))
+            queue.update(set(boxes[key]).difference(visited))
+    return len(visited) == len(boxes)
