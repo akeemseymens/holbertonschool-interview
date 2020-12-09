d57998c6942bf12aa85b2adc449f183d907d5d34
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2020-12-09 13:54:44 -0800

    Update 0-lockboxes.py
    changed for checkers edge cases.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
-"""Find out if the room has all the keys for the room return a boolean."""
+#!/usr/bin python3
+""" Find out if the room has all the keys for the room returns a boolean."""
 
 
 def canUnlockAll(boxes):
   """check if user and unlock all boxes"""
-  
-  queue = [0]
-  visited = {0}
-  while queue:
-    rm = queue.pop() #visited room
-    for key in boxes[rm]:
-      if key in visited:
-        continue #key is already added to the room
-      visited.add(key)
-      queue.append(key)
-  return len(visited) == len(boxes)
+  visited = set()
+  queue = set((0,))
+  while len(queue) > 0:
+    key = queue.pop()
+    if key < len(boxes):
+      visited.update((key,))
+      queue.update(set(boxes[key]).difference(visited))
+    return len(visited) == len(boxes)
