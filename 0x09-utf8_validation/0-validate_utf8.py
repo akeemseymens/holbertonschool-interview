45843ac7e2d4f3d2ef66aef6e1bdef3d82bda8a4
Akeem Seymens <akeems@fb.com>
2021-02-18 23:41:27 -0800

    test format spacing

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
 """
-Write a method that determines if a given data set represents
-a valid UTF-8 encoding
+UTF8 checking function
 """
 
 
 def validUTF8(data):
     """
-    validUTF8 - Determine if a data set represents a valid UTF-8 encoding
-    @data: The data set to 
-    
-    Return: True if valid UTF-8, False otherwise
+    check if array of UTF-8 is valid
     """
-    
-    bytes_behind = 0
+    trailing_bytes = 0
     for byte in data:
-        if bytes_behind:
+        if trailing_bytes:
             if byte >> 6 != 0b10:
                 return False
-            bytes_behind -= 1
+            trailing_bytes -= 1
         else:
             if byte >> 7 == 0:
                 continue
             elif byte >> 3 == 0b11110:
-                bytes_behind = 3
+                trailing_bytes = 3
             elif byte >> 4 == 0b1110:
-                bytes_behind = 2
+                trailing_bytes = 2
             elif byte >> 5 == 0b110:
-                bytes_behind = 1
+                trailing_bytes = 1
             else:
                 return False
-    return bytes_behind == 0
+
+    return trailing_bytes == 0
