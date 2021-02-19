645f3e033cf56b9ba0622ab86450d3bec40cb6b3
Akeem Seymens <akeems@fb.com>
2021-02-18 23:51:55 -0800

    testing the checker

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-"""
-Valid UTF8 function checking
-"""
+'''
+Write a method that determines if a given data set represents
+a valid UTF-8 encoding
+'''
 
 
 def validUTF8(data):
-    """
+    '''
     validUTF8 - Determine if a data set represents a valid UTF-8 encoding
-    @data: The data set to 
-    
+    @data: The data set to evaluate
     Return: True if valid UTF-8, False otherwise
-    """
-    bytes_behind = 0
-    for byte in data:
-        if bytes_behind:
-            if byte >> 6 != 0b10:
-                return False
-            bytes_behind -= 1
-        else:
-            if byte >> 7 == 0:
+    '''
+    n_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for num in data:
+        mask = 1 << 7
+        if n_bytes == 0:
+            while mask & num:
+                n_bytes += 1
+                mask = mask >> 1
+            if n_bytes == 0:
                 continue
-            elif byte >> 3 == 0b11110:
-                bytes_behind = 3
-            elif byte >> 4 == 0b1110:
-                bytes_behind = 2
-            elif byte >> 5 == 0b110:
-                bytes_behind = 1
-            else:
+            if n_bytes == 1 or n_bytes > 4:
                 return False
-    return bytes_behind == 0
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
