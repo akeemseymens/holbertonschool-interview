31e9f59f8f343ea6e0fbe19afaf51562a3c8882c
Akeem Seymens <51678435+akeemseymens@users.noreply.github.com>
2021-09-16 16:14:25 -0700

    added files for project

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,20 @@
+
+#include "palindrome.h"
+
+/**
+ * is_palindrome - check if a positive integer is a palindrome.
+ * @n: a positive integer
+ * Return: 1 if n is a palindrome, else 0
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long a = n, b = 0;
+
+	while (a)
+	{
+		b = b * 10 + (a % 10);
+		a /= 10;
+	}
+
+	return (b == n);
+}
