f72ab1374e49f2cec454338309f374387b2697be
Akeem Seymens <akeems@fb.com>
2022-01-13 21:22:31 -0800

    added project

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - checks for a cycle in a linked list
+ * @head: linked list
+ * Return: 1 if there is a cycle in the list else 0
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
